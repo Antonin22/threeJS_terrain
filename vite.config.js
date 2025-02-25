a3c5ae082aafd0d4972ec003e85df337c2975127
Antonin22 <158173168+Antonin22@users.noreply.github.com>
2025-02-25 13:16:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/three_vite/",
+    base: "/threeJS_terrain/",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
