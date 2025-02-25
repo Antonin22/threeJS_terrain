d1baa0ad65d987643311b4d138ce593fe20d7ccc
Antonin22 <formation@ubuntu.ubuntu-domain>
2025-02-25 15:34:16 +0100

    fix import

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/threeJS_terrain/",
+    base: "/threeJS_terrain",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
